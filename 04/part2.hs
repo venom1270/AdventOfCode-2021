2b3c44d961f2ab8990ea489df549d97376129ddd
Žiga S <zigsim1@gmail.com>
2021-12-04 15:15:56 +0100

    Day 4

---
+++ 04/part2.hs
@@ -0,0 +1,89 @@
+import System.IO
+import Control.Monad
+import GHC.Read (list)
+import Debug.Trace
+import Data.List (elemIndex)
+import Data.Maybe (fromMaybe)
+
+
+bingoBoardMarkNumber :: [Int] -> Int -> [Int]
+bingoBoardMarkNumber [] _ = []
+bingoBoardMarkNumber (h:t) n
+        | h == n = -1 : bingoBoardMarkNumber t n
+        | otherwise = h : bingoBoardMarkNumber t n
+
+bingoBoardsMarkNumbers :: [[Int]] -> Int -> [[Int]]
+bingoBoardsMarkNumbers x n = map (`bingoBoardMarkNumber` n) x
+
+
+bingoGame :: [[Int]] -> [Int] -> Int
+bingoGame boards numbersList
+        | any (==True) check && length check == 1 = number * sum (filter (>= 0) (head markedBoards))
+        | any (==True) check = bingoGame (removeBoards (zip markedBoards check)) numbers
+        | otherwise = bingoGame markedBoards numbers
+        where markedBoards = bingoBoardsMarkNumbers boards number
+              check = map checkBoard markedBoards
+              number = head numbersList
+              numbers = tail numbersList
+
+checkBoard :: [Int] -> Bool
+checkBoard x = checkRows x || checkCols x 0
+
+checkRows :: [Int] -> Bool
+checkRows [] = False
+checkRows x = all (== -1) (take 5 x) || checkRows (drop 5 x)
+
+checkCols :: [Int] -> Int -> Bool
+checkCols _ 5 = False
+checkCols x n = all (== -1) (everyNth x n) || checkCols x (n+1)
+
+everyNth :: [Int] -> Int -> [Int]
+everyNth [] _ = []
+everyNth x n = take 5 x !! n : everyNth (drop 5 x) n
+
+removeBoards :: [([Int], Bool)] -> [[Int]]
+removeBoards b = map fst (filter (\(_,x) -> not x ) b)
+
+main = do
+        let list = []
+        handle <- openFile "input_test.txt" ReadMode
+        contents <- hGetContents handle
+        let singlewords = words contents
+        let drawnNumbers = readDrawnNumbers (head singlewords)
+        let bingoNumbersIntList = stringListToInt (tail singlewords)
+        let bingoBoards = toBingoBoards bingoNumbersIntList 5
+        --print (checkCols (head bingoBoards) 5)
+        --print drawnNumbers
+        --print bingoNumbersIntList
+        print (bingoGame bingoBoards drawnNumbers)
+
+
+readDrawnNumbers :: String -> [Int]
+readDrawnNumbers x = map read (split ',' x)
+
+inputToTuples :: [String] -> [Int]
+inputToTuples (h1:t) = read h1: inputToTuples t
+inputToTuples _ = []
+
+stringListToInt :: [String] -> [Int]
+stringListToInt = map read
+
+toBingoBoards :: [Int] -> Int -> [[Int]]
+toBingoBoards [] _ = []
+toBingoBoards x size = take (size*size) x : toBingoBoards (drop (size*size) x) size
+
+-- From stackoverflow
+split :: Char -> String -> [String]
+split _ "" = []
+split c s = firstWord : (split c rest)
+    where firstWord = takeWhile (/=c) s
+          rest = drop (length firstWord + 1) s
+
+{-
+toBingoBoardArrays :: [Int] -> [[[Int]]]
+toBingoBoardArrays x boardSize = toBingoBoard (take boardSize*boardSize x) boardSize : toBingoBoardArrays (drop boardSize*boardSize x) boardSize
+
+toBingoBoard :: [Int] -> Int -> [[Int]]
+toBingoBoard [] _ = []
+toBingoBoard x size = drop size x : toBingoBoard (drop size x) size
+-}
--- README.md
+++ README.md
@@ -5,4 +5,5 @@ I'll try to do this in Haskell 😎... wish me luck.
  - Day 1: ✅ (why did I choose Haskell... it's gonna be a long month)
  - Day 2: ✅ (that was... surprisingly easy... I'm gonna credit that to my exceptional Haskell skillz 😎)
  - Day 3: ✅ (part 1 was quite easy.... part 2 took wayy too much time (hours)...should've read the puzzle instructions in full... GARBAGE CODE - BUT IT WORKS!)
+- Day 4: ✅ (code is much better, much more readable 😁 part 1 was tough to setup every utility function and so on but it was worth it in the end - part 2 was cake 😎; the pattern I'm seeing here is: on odd days it's a nightmare, while even days are a little bit more manageable)
 
